3a8d4bba92109e6551acd545d284fb90cca60c12
smudesign <urwinsamantha@gmail.com>
2021-10-25 15:00:49 -0400

    PORT

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const e = require('express');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json');
 
@@ -44,6 +45,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 })
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
